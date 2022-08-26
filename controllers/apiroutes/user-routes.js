6863f624a8cf25b33d41daddf8a0951d8c57dad3
samvrny <samvrny@gmail.com>
2022-08-26 14:25:41 -0500

    got post route for new users operational

--- controllers/apiroutes/user-routes.js
+++ controllers/apiroutes/user-routes.js
@@ -13,4 +13,18 @@ router.get('/', (req, res) => {
     });
 });
 
+//add get route for getting all users by id
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData)) //will add session data here later
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
 module.exports = router;
