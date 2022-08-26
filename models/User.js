3ef98bc70e0c69a77929beff9324f69e085a0b05
samvrny <samvrny@gmail.com>
2022-08-26 13:44:17 -0500

    build the user model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt'); //NOTE: Will need to refer back to User.js from just TN
+//and hash all of the passwords. May need to refer to the modules in order to
+//Get a better understanding of what that might entail.
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        }
+    },
+    {
+        hooks: {
+            //add password authentication here
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
