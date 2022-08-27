9f3d40f11f3f9eba571d0f17fe1d25b24089325e
samvrny <samvrny@gmail.com>
2022-08-26 23:10:45 -0500

    got the get and post route for the posts done. post model complete

--- controllers/apiroutes/post-routes.js
+++ controllers/apiroutes/post-routes.js
@@ -2,4 +2,35 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User } = require('../../models');
 
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'post_contents', 'user_id', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_contents: req.body.post_contents,
+        user_id: req.body.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
