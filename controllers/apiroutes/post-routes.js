559261be86fa4c7d2e0fbf6f72c7a39b00d2c558
samvrny <samvrny@gmail.com>
2022-08-27 14:36:01 -0500

    finally got the comment data to GRAB now I just have to print it to the page

--- controllers/apiroutes/post-routes.js
+++ controllers/apiroutes/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -10,8 +10,45 @@ router.get('/', (req, res) => {
             {
                 model: User,
                 attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
             }
         ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'post_contents', 'user_id', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes:['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ] 
     })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
